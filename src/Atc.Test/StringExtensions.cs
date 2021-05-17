7876b6908adf46de74f503b682e7513550f491ce
Ricky Kaare Engelharth <ren@delegate.dk>
2021-05-17 10:23:33 +0200

    Add functionality for comparing string without formatting

---
+++ src/Atc.Test/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using FluentAssertions.Primitives;
+
+namespace Atc.Test
+{
+    public static class StringExtensions
+    {
+        public static void BeSameContentAs(
+            this StringAssertions assertions,
+            string expected,
+            string because = "",
+            params string[] becauseArgs)
+            => assertions.Subject.WithoutFormatting()
+                .Should()
+                .BeEquivalentTo(expected.WithoutFormatting(), because, becauseArgs);
+
+        public static void NotBeSameContentAs(
+            this StringAssertions assertions,
+            string expected,
+            string because = "",
+            params string[] becauseArgs)
+            => assertions.Subject.WithoutFormatting()
+                .Should()
+                .NotBeEquivalentTo(expected.WithoutFormatting(), because, becauseArgs);
+
+        public static string WithoutFormatting(
+            this string str)
+            => Regex.Replace(
+                str.Trim(),
+                "[\\s\\n]+",
+                " ",
+                RegexOptions.None,
+                TimeSpan.FromSeconds(1));
+    }
+}
---
+++ test/Atc.Test.Tests/StringExtensionsTests.cs
@@ -0,0 +1,35 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Atc.Test.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("Double  Space", "Double Space")]
+        [InlineData("Triple   Space", "Triple Space")]
+        [InlineData("Line \n Break", "Line Break")]
+        [InlineData(" \n  Leading spaces", "Leading spaces")]
+        [InlineData("Trailing spaces  \n ", "Trailing spaces")]
+        public void WithoutFormatting_Should_Remove_Duplicate_Whitespaces_And_Trim(
+            string input,
+            string expectedResult)
+            => input.WithoutFormatting()
+                .Should()
+                .Be(expectedResult.WithoutFormatting());
+
+        [Theory]
+        [InlineData("Double  Space", "Double Space")]
+        [InlineData("Triple   Space", "Triple Space")]
+        [InlineData("Line \n Break", "Line Break")]
+        [InlineData(" \n  Leading spaces", "Leading spaces")]
+        [InlineData("Trailing spaces  \n ", "Trailing spaces")]
+        public void BeSameContentAs_Should_Compare_String_WithoutFormatting(
+            string input,
+            string expectedResult)
+            => input
+                .Should()
+                .BeSameContentAs(expectedResult);
+    }
+}
