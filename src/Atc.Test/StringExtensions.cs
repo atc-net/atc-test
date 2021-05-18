01684c1d8109eab7c12a84d42030d692c5f15664
Ricky Kaare Engelharth <ren@delegate.dk>
2021-05-18 08:08:06 +0200

    Add functionality for comparing strings with xml and json disregarding the formatting

--- src/Atc.Test/StringExtensions.cs
+++ src/Atc.Test/StringExtensions.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Buffers;
+using System.IO;
+using System.Text;
+using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Xml;
 using FluentAssertions;
 using FluentAssertions.Primitives;
 
@@ -12,7 +17,7 @@ namespace Atc.Test
         /// </summary>
         /// <param name="assertions">The StringAssertions.</param>
         /// <param name="expected">
-        /// The string that the subject is expected to be similar in content to.
+        /// The string that the subject is expected to have similar content as.
         /// </param>
         /// <param name="because">
         /// A formatted phrase as is supported by <see cref="string.Format(string, object[])" /> explaining why the assertion
@@ -21,7 +26,7 @@ namespace Atc.Test
         /// <param name="becauseArgs">
         /// Zero or more objects to format using the placeholders in <paramref name="because"/>.
         /// </param>
-        public static void BeSimilarContentAs(
+        public static void HaveSimilarContentAs(
             this StringAssertions assertions,
             string expected,
             string because = "",
@@ -31,11 +36,11 @@ namespace Atc.Test
                 .BeEquivalentTo(expected.WithoutFormatting(), because, becauseArgs);
 
         /// <summary>
-        /// Asserts that a string has similar content disregarding formatting and casing.
+        /// Asserts that a string does not have similar content disregarding formatting and casing.
         /// </summary>
         /// <param name="assertions">The StringAssertions.</param>
         /// <param name="expected">
-        /// The string that the subject is expected to be similar in content to.
+        /// The string that the subject is expected not to have similar content as.
         /// </param>
         /// <param name="because">
         /// A formatted phrase as is supported by <see cref="string.Format(string,object[])" /> explaining why the assertion
@@ -44,7 +49,7 @@ namespace Atc.Test
         /// <param name="becauseArgs">
         /// Zero or more objects to format using the placeholders in <paramref name="because"/>.
         /// </param>
-        public static void NotBeSimilarContentAs(
+        public static void NotHaveSimilarContentAs(
             this StringAssertions assertions,
             string expected,
             string because = "",
@@ -52,6 +57,106 @@ namespace Atc.Test
             => assertions.Subject.WithoutFormatting()
                 .Should()
                 .NotBeEquivalentTo(expected.WithoutFormatting(), because, becauseArgs);
+
+        /// <summary>
+        /// Asserts that a string has similar xml content disregarding formatting.
+        /// </summary>
+        /// <param name="assertions">The StringAssertions.</param>
+        /// <param name="expected">
+        /// The string that the subject is expected to have similar xml content as.
+        /// </param>
+        /// <param name="because">
+        /// A formatted phrase as is supported by <see cref="string.Format(string, object[])" /> explaining why the assertion
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        /// Zero or more objects to format using the placeholders in <paramref name="because"/>.
+        /// </param>
+        public static void HaveSimilarXmlAs(
+            this StringAssertions assertions,
+            string expected,
+            string because = "",
+            params string[] becauseArgs)
+        {
+            assertions.Subject.WithXmlFormatting()
+                .Should()
+                .Be(expected.WithXmlFormatting(), because, becauseArgs);
+        }
+
+        /// <summary>
+        /// Asserts that a string does not have similar xml content disregarding formatting.
+        /// </summary>
+        /// <param name="assertions">The StringAssertions.</param>
+        /// <param name="expected">
+        /// The string that the subject is expected not to have similar xml content as.
+        /// </param>
+        /// <param name="because">
+        /// A formatted phrase as is supported by <see cref="string.Format(string, object[])" /> explaining why the assertion
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        /// Zero or more objects to format using the placeholders in <paramref name="because"/>.
+        /// </param>
+        public static void NotHaveSimilarXmlAs(
+            this StringAssertions assertions,
+            string expected,
+            string because = "",
+            params string[] becauseArgs)
+        {
+            assertions.Subject.WithXmlFormatting()
+                .Should()
+                .NotBe(expected.WithXmlFormatting(), because, becauseArgs);
+        }
+
+        /// <summary>
+        /// Asserts that a string has similar json content disregarding formatting.
+        /// </summary>
+        /// <param name="assertions">The StringAssertions.</param>
+        /// <param name="expected">
+        /// The string that the subject is expected to have similar json contnet as.
+        /// </param>
+        /// <param name="because">
+        /// A formatted phrase as is supported by <see cref="string.Format(string, object[])" /> explaining why the assertion
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        /// Zero or more objects to format using the placeholders in <paramref name="because"/>.
+        /// </param>
+        public static void HaveSimilarJsonAs(
+           this StringAssertions assertions,
+           string expected,
+           string because = "",
+           params string[] becauseArgs)
+        {
+            assertions.Subject.WithJsonFormatting()
+                .Should()
+                .Be(expected.WithJsonFormatting(), because, becauseArgs);
+        }
+
+        /// <summary>
+        /// Asserts that a string dos not have similar json content disregarding formatting.
+        /// </summary>
+        /// <param name="assertions">The StringAssertions.</param>
+        /// <param name="expected">
+        /// The string that the subject is expected to not have similar json contnet as.
+        /// </param>
+        /// <param name="because">
+        /// A formatted phrase as is supported by <see cref="string.Format(string, object[])" /> explaining why the assertion
+        /// is needed. If the phrase does not start with the word <i>because</i>, it is prepended automatically.
+        /// </param>
+        /// <param name="becauseArgs">
+        /// Zero or more objects to format using the placeholders in <paramref name="because"/>.
+        /// </param>
+        public static void NotHaveSimilarJsonAs(
+           this StringAssertions assertions,
+           string expected,
+           string because = "",
+           params string[] becauseArgs)
+        {
+            assertions.Subject.WithJsonFormatting()
+                .Should()
+                .NotBe(expected.WithJsonFormatting(), because, becauseArgs);
+        }
 
         /// <summary>
         /// Returns the string with formatting removed.
@@ -66,5 +171,48 @@ namespace Atc.Test
                 " ",
                 RegexOptions.None,
                 TimeSpan.FromSeconds(1));
+
+        /// <summary>
+        /// Returns the string with xml formatting.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns>The string with xml formatting.</returns>
+        public static string WithXmlFormatting(
+            this string str)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(str);
+
+            using var writer = new StringWriter();
+            using var textWriter = new XmlTextWriter(writer)
+            {
+                Formatting = Formatting.Indented,
+            };
+            doc.WriteTo(textWriter);
+
+            return writer.ToString();
+        }
+
+        /// <summary>
+        /// Returns the string with json formatting.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns>The string with json formatting.</returns>
+        public static string WithJsonFormatting(
+            this string str)
+        {
+            var doc = JsonDocument.Parse(str);
+            var writeOptions = new JsonWriterOptions
+            {
+                Indented = true,
+            };
+
+            var buffer = new ArrayBufferWriter<byte>();
+            using var writer = new Utf8JsonWriter(buffer, writeOptions);
+            doc.WriteTo(writer);
+            writer.Flush();
+
+            return Encoding.UTF8.GetString(buffer.WrittenSpan);
+        }
     }
 }
--- test/Atc.Test.Tests/StringExtensionsTests.cs
+++ test/Atc.Test.Tests/StringExtensionsTests.cs
@@ -25,11 +25,53 @@ namespace Atc.Test.Tests
         [InlineData("Line \n Break", "Line Break")]
         [InlineData(" \n  Leading spaces", "Leading spaces")]
         [InlineData("Trailing spaces  \n ", "Trailing spaces")]
-        public void BeSimilarContentAs_Should_Compare_String_WithoutFormatting(
+        public void HaveSimilarContentAs_Should_Compare_String_WithoutFormatting(
             string input,
             string expectedResult)
             => input
                 .Should()
-                .BeSimilarContentAs(expectedResult);
+                .HaveSimilarContentAs(expectedResult);
+
+        [Theory]
+        [InlineData("<xml><a>test</a></xml>", "<xml>\r\t<a>test</a>\n</xml>")]
+        [InlineData("<xml    test=\"test\"></xml>", "<xml test=\"test\"></xml>")]
+        public void WithXmlFormatting_Should_Align_Xml_Formatting(
+            string input,
+            string expectedResult)
+            => input.WithXmlFormatting()
+                .Should()
+                .Be(expectedResult.WithXmlFormatting());
+
+        [Theory]
+        [InlineData("<xml><a>test</a></xml>", "<xml>\r\t<a>test</a>\n</xml>")]
+        [InlineData("<xml    test=\"test\"></xml>", "<xml test=\"test\"></xml>")]
+        public void HaveSimilarXmlAs_Should_Compare_String_WithoutFormatting(
+            string input,
+            string expectedResult)
+            => input
+                .Should()
+                .HaveSimilarXmlAs(expectedResult);
+
+        [Theory]
+        [InlineData("{}", "{  \n\t}")]
+        [InlineData("[]", "[  \n\t]")]
+        [InlineData("{\"test\":\"value\"}", "{\n\t\"test\": \"value\"\n}")]
+        public void WithJsonFormatting_Should_Align_Xml_Formatting(
+            string input,
+            string expectedResult)
+            => input.WithJsonFormatting()
+                .Should()
+                .Be(expectedResult.WithJsonFormatting());
+
+        [Theory]
+        [InlineData("{}", "{  \n\t}")]
+        [InlineData("[]", "[  \n\t]")]
+        [InlineData("{\"test\":\"value\"}", "{\n\t\"test\": \"value\"\n}")]
+        public void HaveSimilarJsonAs_Should_Compare_String_WithoutFormatting(
+            string input,
+            string expectedResult)
+            => input
+                .Should()
+                .HaveSimilarJsonAs(expectedResult);
     }
 }
