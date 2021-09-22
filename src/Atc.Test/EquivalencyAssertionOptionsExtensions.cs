b99ce1a42b1a98de110ad61129160ba1d6f4373c
Ricky Kaare Engelharth <ren@delegate.dk>
2021-09-22 19:26:54 +0200

    Update dependencies

--- src/Atc.Test/EquivalencyAssertionOptionsExtensions.cs
+++ src/Atc.Test/EquivalencyAssertionOptionsExtensions.cs
@@ -24,6 +24,28 @@ namespace Atc.Test
             => (options ?? throw new ArgumentNullException(nameof(options)))
                 .Using<DateTimeOffset>(ctx => ctx.Subject
                     .Should()
+                    .BeCloseTo(ctx.Expectation, TimeSpan.FromMilliseconds(precision)))
+                .WhenTypeIs<DateTimeOffset>()
+                .Using<DateTime>(ctx => ctx.Subject
+                    .Should()
+                    .BeCloseTo(ctx.Expectation, TimeSpan.FromMilliseconds(precision)))
+                    .WhenTypeIs<DateTime>();
+
+        /// <summary>
+        /// Configures .BeEquivalentTo extensions to compare <see cref="DateTime"/> and
+        /// <see cref="DateTimeOffset "/> values by checking if they are within the specified
+        /// number of milliseconds (default = 1s).
+        /// </summary>
+        /// <typeparam name="T">The generic parameter for the <see cref="EquivalencyAssertionOptions{T}"/>.</typeparam>
+        /// <param name="options">The <see cref="EquivalencyAssertionOptions{T}"/> to configure.</param>
+        /// <param name="precision">The precision.</param>
+        /// <returns>The configured <see cref="EquivalencyAssertionOptions{T}"/>.</returns>
+        public static EquivalencyAssertionOptions<T> CompareDateTimeUsingCloseTo<T>(
+            this EquivalencyAssertionOptions<T> options,
+            TimeSpan precision)
+            => (options ?? throw new ArgumentNullException(nameof(options)))
+                .Using<DateTimeOffset>(ctx => ctx.Subject
+                    .Should()
                     .BeCloseTo(ctx.Expectation, precision))
                 .WhenTypeIs<DateTimeOffset>()
                 .Using<DateTime>(ctx => ctx.Subject
