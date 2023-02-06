4a57e5ec35cf9aad08ad89572271368752db3287
Thomas Malowanczyk <tkm@delegate.dk>
2023-02-06 08:19:59 +0100

    fix: memberdata removes parameters with same values
    The current MemberAutoNSubstituteDataAttribute implementation uses Union to merge parameters form a MemberData paramters and AutoFixtured and NSubstituted parameters. But Union has the side effect of removing duplicate values. Replace Union with Concat instead.

--- test/Atc.Test.Tests/MemberAutoNSubstituteDataAttributeTests.cs
+++ test/Atc.Test.Tests/MemberAutoNSubstituteDataAttributeTests.cs
@@ -25,4 +25,29 @@ public class MemberAutoNSubstituteDataAttributeTests
         dependantType.Should().NotBeNull();
         dependantType.Dependency.Should().Be(interfaceType);
     }
+
+    public static readonly IEnumerable<object[]> TestDataMultiple = new[]
+    {
+        new object[] { SampleEnum.One, SampleEnum.Two },
+        new object[] { SampleEnum.One, SampleEnum.One },
+    };
+
+    [Theory]
+    [MemberAutoNSubstituteData(nameof(TestDataMultiple))]
+    public void MemberAutoNSubstituteData_Should_Call_For_Each_Value_Mutliple(
+        SampleEnum value1,
+        SampleEnum value2,
+        [Frozen] ISampleInterface interfaceType,
+        SampleClass concreteType,
+        SampleDependantClass dependantType)
+    {
+        value1.Should().BeOneOf(SampleEnum.One, SampleEnum.Two, SampleEnum.Three);
+        value2.Should().BeOneOf(SampleEnum.One, SampleEnum.Two, SampleEnum.Three);
+        interfaceType.Should().NotBeNull();
+        interfaceType.IsSubstitute().Should().BeTrue();
+        concreteType.Should().NotBeNull();
+        concreteType.IsSubstitute().Should().BeFalse();
+        dependantType.Should().NotBeNull();
+        dependantType.Dependency.Should().Be(interfaceType);
+    }
 }
