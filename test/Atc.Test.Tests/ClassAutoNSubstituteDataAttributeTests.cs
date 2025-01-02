9df4e948435fef0c5d56fc8080f6d856afd91b64
Ricky Kaare Engelharth <ricky@ricky.dk>
2025-01-02 13:13:18 +0100

    Fix issue with ClassAutoNSubstituteDataAttribute when using FrozenAtrribute

---
+++ test/Atc.Test.Tests/ClassAutoNSubstituteDataAttributeTests.cs
@@ -0,0 +1,31 @@
+namespace Atc.Test.Tests;
+
+public class ClassAutoNSubstituteDataAttributeTests
+{
+    public class TestData : TheoryData<SampleEnum>
+    {
+        public TestData()
+        {
+            AddRow(SampleEnum.One);
+            AddRow(SampleEnum.Two);
+            AddRow(SampleEnum.Three);
+        }
+    }
+
+    [Theory]
+    [ClassAutoNSubstituteData(typeof(TestData))]
+    public void MemberAutoNSubstituteData_Should_Call_For_MemberData(
+        SampleEnum value,
+        [Frozen] ISampleInterface interfaceType,
+        SampleClass concreteType,
+        SampleDependantClass dependantType)
+    {
+        value.Should().BeOneOf(SampleEnum.One, SampleEnum.Two, SampleEnum.Three);
+        interfaceType.Should().NotBeNull();
+        interfaceType.IsSubstitute().Should().BeTrue();
+        concreteType.Should().NotBeNull();
+        concreteType.IsSubstitute().Should().BeFalse();
+        dependantType.Should().NotBeNull();
+        dependantType.Dependency.Should().Be(interfaceType);
+    }
+}
