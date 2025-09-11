0f5e8fac83e16ef9d7361093d8ff4ebdf8897048
Per Kops <per_kops@hotmail.com>
2025-09-11 21:55:35 +0200

    feat: upgrade dependencies to xunit v3

--- src/Atc.Test/GlobalUsings.cs
+++ src/Atc.Test/GlobalUsings.cs
@@ -6,21 +6,17 @@ global using System.Text;
 global using System.Text.Json;
 global using System.Text.RegularExpressions;
 global using System.Xml;
-
 global using Atc.Test.Customizations;
-
 global using AutoFixture;
 global using AutoFixture.AutoNSubstitute;
 global using AutoFixture.Kernel;
-global using AutoFixture.Xunit2;
-
+global using AutoFixture.Xunit3;
 global using FluentAssertions;
 global using FluentAssertions.Equivalency;
 global using FluentAssertions.Primitives;
-
 global using NSubstitute;
 global using NSubstitute.Core;
 global using NSubstitute.ReceivedExtensions;
-
 global using Xunit;
 global using Xunit.Sdk;
+global using Xunit.v3;
--- test/Atc.Test.Tests/GlobalUsings.cs
+++ test/Atc.Test.Tests/GlobalUsings.cs
@@ -1,1 +1,2 @@
 global using Atc.Test.Tests.SampleTypes;
+global using AutoFixture.Xunit3;
